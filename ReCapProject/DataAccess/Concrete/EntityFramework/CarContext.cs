b80de4e750bfaf83110749f99df63c5136d99a01
icivioglu <icivioglu@gmail.com>
2021-02-06 11:31:34 +0300

    Updated

---
+++ ReCapProject/DataAccess/Concrete/EntityFramework/CarContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CarContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"(localdb)\MSSQLLocalDB;Database=RentCar;Trusted_Connection=true");
+        }
+    }
+}
---
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+                new Car{CarId=1, BrandId=2, ColorId=2, ModelYear = 2015, DailyPrice=1000, Description = "Araç hatasız boyasız hasar kayıtsız" },
+                new Car{CarId=2, BrandId=2, ColorId=1, ModelYear = 2010, DailyPrice=800, Description = "Hatasız boyasız " },
+                new Car{CarId=3, BrandId=1, ColorId=2, ModelYear = 2020, DailyPrice=5000, Description = "Sıfır ayarında" },
+                new Car{CarId=4, BrandId=3, ColorId=3, ModelYear = 2015, DailyPrice=1000, Description = "Hasar kayıtsız" },
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {          
+            Car carToDelete = _cars.SingleOrDefault(c=>c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.CarId == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
